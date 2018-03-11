8f4e1105367a15f37aeec26566e7f77ca40b8506
Tanmay Nath <tanmaynath@Tanmays-MacBook-Pro.local>
2018-03-11 14:32:35 +0100

    First commit. Works well in daylight

---
+++ Assets/CarController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour {
+
+	private bool soundFlag = false;
+	
+	// Update is called once per frame
+	void Update () {
+
+		print("Flag: " + !soundFlag + " local Position: " + transform.localPosition.y);
+		if(!soundFlag && transform.localPosition.y < 0.05f)
+		{
+			soundFlag = true;
+			StartCoroutine(DelayedSoundPlay ());
+			Debug.Log("Started Coroutine");
+
+		}
+	}
+
+	public void MoveCar()
+	{
+		Debug.Log("move car called");
+		transform.localPosition += new Vector3 (0, 10.0f, 0);
+		transform.eulerAngles = new Vector3 (5, 20, 5);
+		soundFlag = false;
+	}
+
+	IEnumerator DelayedSoundPlay ()
+	{
+		Debug.Log("in coroutine");
+		yield return new WaitForSeconds(0.2f);
+		GetComponent <AudioSource>().Play();
+		Debug.Log("sound played!");
+	}
+}
---
+++ Assets/DoorBehaviour.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorBehaviour : MonoBehaviour {
+
+
+	private float currAngle = 0.0f;
+	private float desiredAngle = 0.0f;
+	
+	// Update is called once per frame
+	void Update () {
+		currAngle = Mathf.LerpAngle(currAngle, desiredAngle, Time.deltaTime * 3.0f);
+		transform.localEulerAngles = new Vector3(0, 0, currAngle);
+	}
+
+	void OpenDoors()
+	{
+		if(gameObject.CompareTag("RightDoor"))
+		{
+			desiredAngle = 60.0f;
+		}
+		else 
+		{
+			desiredAngle = -60.0f;
+		}
+		GetComponent <AudioSource>().Play();
+
+	}
+
+	void CloseDoors()
+	{
+		desiredAngle = 0.0f;
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.CompareTag("MainCamera"))
+		{
+			OpenDoors();
+
+		}
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		if(other.CompareTag("MainCamera"))
+		{
+			CloseDoors();
+		}
+	}
+}
